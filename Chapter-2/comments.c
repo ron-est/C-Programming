fc99db1cf3bc3bfb44b0c37914e6a123218c574e
Ronald Esteban <ronaldesteban@Ronalds-MacBook-Air.local>
2025-12-18 22:37:34 -0800

    new comments

---
+++ Chapter-2/comments.c
@@ -0,0 +1,23 @@
+/* Name: comments.c*/
+/* Purpose: prints a bad pun. */
+/* Author: K. N. King */
+
+/* These types of comments
+can extend past one line */
+
+/*
+* This is
+* another way
+* to have a multi-line comment
+*/
+
+// This is also another way to type a comment
+// This was brough about in C99
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C, or not to C: that is the question.\n");
+    return 0;
+}
