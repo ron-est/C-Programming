e083c96682c2ed254185ceb2c169f00ab4f5755b
Ronald Esteban <ronaldesteban@Ronalds-MacBook-Air.local>
2025-12-18 20:41:44 -0800

    section 2.2

--- Chapter-2/pun.c
+++ Chapter-2/pun.c
@@ -1,9 +1,7 @@
-// C's standard input and output library
 #include <stdio.h>
 
 int main(void)
 {
     printf("To C, or not to C: that is the question.\n");
-    // Indicates that the program "returns" the value 0 to the OS when it terminates
     return 0;
 }
