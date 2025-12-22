f41c4aad5a62b82b9835afa854dc1df490d2719f
ron.est <ronaldbozesteban@gmail.com>
2025-12-22 13:07:24 -0800

    project_2

---
+++ Chapter-2/project_2.c
@@ -0,0 +1,16 @@
+/* Write a program that computes the volume of a sphere with a 10-meter radius */
+
+#include <stdio.h>
+
+#define PI 3.14f
+
+int main(void)
+{
+    float radius;
+
+    printf("Enter radius of the sphere: \n");
+    scanf("%f", &radius);
+    float volume = (4.0f / 3.0f) * PI * radius * radius * radius;
+    printf("Volume: %.2f\n", volume);
+    return 0;
+}
