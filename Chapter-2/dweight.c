34fc9d17007bd9bebfb8ace5f15cb1e1a20bd8b4
ron.est <ronaldbozesteban@gmail.com>
2025-12-22 11:18:38 -0800

    dweight

---
+++ Chapter-2/dweight.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int height, length, width, volume, weight;
+
+    height = 8;
+    length = 12;
+    width = 10;
+    volume = height * length * width;
+    weight = (volume + 165) / 166;
+
+    printf("Dimenson: %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+    return 0;
+}
