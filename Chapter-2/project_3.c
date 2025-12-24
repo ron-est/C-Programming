81c28d43f2c856b29f2433e1389be06df8c94121
ron.est <ronaldbozesteban@gmail.com>
2025-12-23 22:16:37 -0800

    project_3

---
+++ Chapter-2/project_3.c
@@ -0,0 +1,16 @@
+// Write ap rogram that asks the user to enter a dollars-and-cents amount
+// Then displays the amount with 5% tax added
+
+#include <stdio.h>
+#define TAX 0.05f
+
+int main(void)
+{
+    float amount;
+
+    printf("Enter an amount: \n");
+    scanf("%f", &amount);
+    float total = TAX * amount + amount;
+    printf("With tax added: %.2f\n", total);
+    return 0;
+}
