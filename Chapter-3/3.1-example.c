567d42e587c386743a6327537e229f17107a430d
ron.est <ronaldbozesteban@gmail.com>
2025-12-27 09:34:20 -0800

    refactor: added comments and examples covering conversion specifications and escape sequences

--- Chapter-3/3.1-example.c
+++ Chapter-3/3.1-example.c
@@ -1,4 +1,4 @@
-// Example code on page 38
+// Example code in Section 3.1
 
 #include <stdio.h>
 
@@ -12,7 +12,22 @@ int main(void)
     x = 43.2892f;
     y = 5527.0f;
 
+    // The ordinary characters in the format string are simply copied to the output line
+    // The four conversion specifications are replaced by the values of the variables i, j, x, and y, in that order
     printf(" i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);
+
+    // Escape Sequences
+    // Alert (bell) \a
+    // Backspace \b
+    // New line \n
+    // Horizontal tab \t
+    printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
+
+    // \" represents the " character
+    printf("\"Hello!\"\n");
+
+    // To print a single \ character, put two \ characters in the string
+    printf("\\\n");
 
     return 0;
 }
