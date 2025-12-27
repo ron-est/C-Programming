35ac7eec98b42a7cf4d5d06104a8c8886842b26a
ron.est <ronaldbozesteban@gmail.com>
2025-12-27 09:14:01 -0800

    refactor: added comments to tprintf.c that explain the different conversion specifications

---
+++ Chapter-3/3.1-example.c
@@ -0,0 +1,18 @@
+// Example code on page 38
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    float x, y;
+
+    i = 10;
+    j = 20;
+    x = 43.2892f;
+    y = 5527.0f;
+
+    printf(" i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);
+
+    return 0;
+}
--- Chapter-3/tprintf.c
+++ Chapter-3/tprintf.c
@@ -10,7 +10,15 @@ int main(void)
     i = 40;
     x = 839.21f;
 
+    // %d - Displays i in decimal form, using a minimum amount of space
+    // %5d - Displays i in decimal form, using a minimum of five characters. Since i requires only two characters, three spaces were added
+    // %-5d - Displays i in decimal form, using a minimum of five characters; since the value of i doesn't require five characters, the spaces are added afterward (that is, i is left-justified in a field of length five)
+    // %5.3d - Displays i in decimal form, using a minimum of five characters overall and a minimum of three digits. Since i is only two digits long, an extra zero was added to guarantee three digits
     printf("| %d | %5d | %-5d | %5.3d |\n", i, i, i, i);
+
+    // %10.3f - Displays x in fixed decimal form, using 10 characters overall, with three decimals after the decimal point
+    // %10.3e - Displays x in exponential form, using 10 characters overall, with three digits after the decimal point
+    // %-10g - Displays x in either fixed decimal form or exponential form, using 10 characters overall
     printf("| %10.3f | %10.3e | %-10g |\n", x, x, x);
 
     return 0;
