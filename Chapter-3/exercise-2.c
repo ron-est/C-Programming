ded3e6b9f7d921988a66dd684e02910b9e9acd50
ron.est <ronaldbozesteban@gmail.com>
2026-01-07 23:16:19 -0800

    feat: added a print function that prints in fixed decimal notation that is left-justified with a field size of 8 and is 3 digits after the decimal point

--- Chapter-3/exercise-2.c
+++ Chapter-3/exercise-2.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    float exponent_number = 1234.567;
+    float exponent_number = 1234.567, decimal_number = 2345.678;
     // Exponential notation; left-justified in a field of size 8; one digit after the decimal point
     printf("%-8.1e\n", exponent_number);
 
@@ -13,7 +13,7 @@ int main(void)
     printf("%10.6e\n", exponent_number);
 
     // Fixed decimal notation; left-justified in a field of size 8; three digits after the decimal point
-
+    printf("%8.3d\n", decimal_number);
 
     // Fixed decimal notation; right-justified in a field of size 6; no digits after the decimal point
 
