fcf2431e8bfc0f8c8ab5c054f5af7d06ca8e9627
ron.est <ronaldbozesteban@gmail.com>
2026-01-09 19:51:14 -0800

    refactor: changed variable name and added decimal print function

--- Chapter-3/exercise-2.c
+++ Chapter-3/exercise-2.c
@@ -4,18 +4,19 @@
 
 int main(void)
 {
-    float exponent_number = 1234.567, decimal_number = 2345.678;
+    float test_number = 1234.567;
     // Exponential notation; left-justified in a field of size 8; one digit after the decimal point
-    printf("%-8.1e\n", exponent_number);
+    printf("%-8.1e\n", test_number);
 
 
     // Exponential notation; right-justtified in a field of size 10; six digits after the decimal point
-    printf("%10.6e\n", exponent_number);
+    printf("%10.6e\n", test_number);
 
     // Fixed decimal notation; left-justified in a field of size 8; three digits after the decimal point
-    printf("%8.3d\n", decimal_number);
+    printf("%8.3d\n", test_number);
 
     // Fixed decimal notation; right-justified in a field of size 6; no digits after the decimal point
+    printf("%6.0d\n", test_number);
 
 
     return 0;
